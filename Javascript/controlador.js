d64d1ea57f1023aa71405453dabdaa91aaaae011
Accel Barreto <accel.5to@gmail.com>
2025-05-04 14:44:12 -0400

    agrege funcion de color

--- Javascript/controlador.js
+++ Javascript/controlador.js
@@ -5,5 +5,11 @@ $(document).ready(function()
         function bienvenida()
             {
                 $('#lbl_bienvenida').html("<h3>Hola mundo</h3>");
+                color_del_labl();
+            }
+
+        function color_del_labl()
+            {
+                $('#lbl_bienvenida').css("background-color", "rgb(250, 250,250)");
             }
     });
